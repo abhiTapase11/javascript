0ba3f08e495d10281b28871232c08d152617b894
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-20 18:34:18 +0000

    null is an object

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treats all js code as newer version of js
+
+// alert("hello") //we are using nodejs not browser
+
+
+// PRIMITIVE DATA TYPES
+// number 
+// bigint
+// string
+// boolean
+// null => empty
+// undefined => we have not assigned any value
+// symbol => uniqueness
+
+// USER DEFINED DATA TYPES
+// object
+
+
+console.log(typeof "abhi")// string
+console.log(typeof 188)// number
+
+console.log(typeof null)// object
+// null is an object in js
+
+console.log(typeof undefined)//undefined
+
