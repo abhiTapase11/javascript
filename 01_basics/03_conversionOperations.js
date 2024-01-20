2b6c972990330efd3dbec46771bb1a47776a6b66
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-20 18:46:45 +0000

    typecasting in js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score="33abc"
+
+console.log(typeof score)//string
+console.log(typeof(score))//string
+
+let value=Number(score)
+/*
+In this way we can typecast into other data types
+Number, Boolean, String, etc.
+*/
+
+console.log(typeof value)//number
+console.log(value)// NaN (NOT A NUMBER)
