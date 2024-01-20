7fcf3e7db932ca5781c217da655236c26f69a2b1
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-20 12:38:29 +0000

    let vs var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let email="abhi@google.com"
+var accountPassword="asdfghj"//var posses scope problem
+// So prefer not to use var
+
+accountCity="Latur"//we can also declare variable like this
+// but it is not a good practice
+
+let accountState
+//it is not necessary to add semicolon at the end of line in js
+
+// accountId=2 // not allowed
+console.log(accountId)
+
+console.table([accountId,email,accountPassword,accountCity,accountState])
