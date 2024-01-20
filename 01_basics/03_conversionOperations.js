ef242586c6e7af006d0a84bdcd87c39b8b56a002
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-20 18:58:36 +0000

    1+2+"2"="32"

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,3 +11,14 @@ Number, Boolean, String, etc.
 
 console.log(typeof value)//number
 console.log(value)// NaN (NOT A NUMBER)
+
+
+
+// *********** Operations ******************
+console.log(3**4)//power
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32 here we first add initial values and then add string
+
