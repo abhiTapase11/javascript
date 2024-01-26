f026fe3ecd604a2ac96f6dabe7ff7da9f69dd00a
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-26 18:24:08 +0000

    function apart from c++

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,8 @@
+  function greet(name){
+    return `Hello ${name}`
+  }
+
+console.log(greet("abhi"));//Hello abhi
+console.log(greet(""));//Hello
+console.log(greet());//Hello undefined
+// as we have not defined any value for name
