955e95880646db3c15089a2d6744a006d23fb439
Abhijit Tapase <95081965+abhiTapase11@users.noreply.github.com>
2024-01-28 14:34:58 +0000

    rest arguments

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,3 +6,15 @@ console.log(greet("abhi"));//Hello abhi
 console.log(greet(""));//Hello
 console.log(greet());//Hello undefined
 // as we have not defined any value for name
+
+function view(...num1){
+  return num1;
+}
+
+console.log(view(3,50,837,9000));//[ 3, 50, 837, 9000 ]
+
+function view2(val1,val2,...num1){
+  return num1;
+}
+
+console.log(view2(3,50,837,9000));//[ 837, 9000 ] as val1=3,val2=50 and rest is in num1
